da07624e6b79d59bf7b8d64f67a3107034d0613f
tallner <ctallner@hotmail.com>
2022-10-12 16:10:47 +0200

    Passing data from one activity to another

--- app/src/main/java/com/example/lifecyclev2/UserDataActivity.kt
+++ app/src/main/java/com/example/lifecyclev2/UserDataActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class UserDataActivity : AppCompatActivity() {
@@ -11,8 +12,15 @@ class UserDataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_data)
 
+        val name = intent.getStringExtra("EXTRA_NAME")
+        val nameout = findViewById<TextView>(R.id.name_output)
+
+        nameout.text = name
+
+
+
         val bottomNav: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav.selectedItemId = R.id.home
+        bottomNav.selectedItemId = R.id.user
 
         bottomNav.setOnItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
--- app/src/main/java/com/example/lifecyclev2/UserSettingsActivity.kt
+++ app/src/main/java/com/example/lifecyclev2/UserSettingsActivity.kt
@@ -4,20 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class UserSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_settings)
+
         val bottomNav: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav.selectedItemId = R.id.home
+        bottomNav.selectedItemId = R.id.settings
+
+        var name = findViewById<TextView>(R.id.name_input)
 
         bottomNav.setOnItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
                 R.id.home -> {
-                    var i = Intent(this, MainActivity::class.java)
-                    startActivity(i)
+
+                    var it = Intent(this, MainActivity::class.java)
+                    startActivity(it)
 
                     true
                 }
@@ -28,8 +33,9 @@ class UserSettingsActivity : AppCompatActivity() {
                 }
 
                 R.id.user -> {
-                    var i = Intent(this, UserDataActivity::class.java)
-                    startActivity(i)
+                    var it = Intent(this, UserDataActivity::class.java)
+                    it.putExtra("EXTRA_NAME",name.text.toString())
+                    startActivity(it)
                     true
                 }
 
