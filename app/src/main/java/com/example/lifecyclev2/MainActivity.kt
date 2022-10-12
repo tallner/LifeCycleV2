434e6c10a480fe306b94d975e2e0575b82255bd3
tallner <ctallner@hotmail.com>
2022-10-12 21:08:20 +0200

    login added but does not save after another activity is started

--- app/src/main/java/com/example/lifecyclev2/MainActivity.kt
+++ app/src/main/java/com/example/lifecyclev2/MainActivity.kt
@@ -1,22 +1,52 @@
 package com.example.lifecyclev2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
-    //private var bottomNav : BottomNavigationView?=null
+    private var loginStatus: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("MyTag", this.lifecycle.currentState.toString())
 
         val bottomNav: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.selectedItemId = R.id.home
+        bottomNav.menu.forEach {
+            it.isEnabled=loginStatus
+            it.isVisible=loginStatus
+        }
+
+        val password: EditText = findViewById(R.id.password_input)
+        val loginButton: Button = findViewById(R.id.btn_login)
+
+
+
+        loginButton.setOnClickListener {
+            loginStatus = password.text.toString().equals("pw")
+            bottomNav.menu.forEach {
+                it.isEnabled=loginStatus
+                it.isVisible=loginStatus
+            }
+        }
+
+
+
+
+
 
         bottomNav.setOnItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
@@ -44,4 +74,25 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("MyTag", this.lifecycle.currentState.toString())
+        outState.putBoolean("loginStatus", loginStatus)
+        Log.i("MyTag", "onSaveInstanceState")
+        Log.i("MyTag", loginStatus.toString())
+        super.onSaveInstanceState(outState)
+
+        Log.i("MyTag", this.lifecycle.currentState.toString())
+
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        loginStatus = savedInstanceState.getBoolean("loginStatus")
+        Log.i("MyTag", "onRestoreInstanceState")
+        Log.i("MyTag", loginStatus.toString())
+    }
+
+
 }
